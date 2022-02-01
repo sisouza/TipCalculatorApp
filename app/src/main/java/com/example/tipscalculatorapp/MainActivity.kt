da5701ef270e89b6a442ba197cc6720a78eba6e1
Sidia <sidia.reis1@gmail.com>
2022-01-31 20:32:16 -0800

    feat:set view binding

--- app/src/main/java/com/example/tipscalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculatorapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.tipscalculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tipscalculatorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    /*declares a top-level variable in the class for the binding object
+    that  will be used across multiple methods in MainActivity class
+     */
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        /* initializes the binding object which will be use to access Views in the activity_main.xml*/
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //specifies the root of the hierarchy of views in app,
+        setContentView(binding.root)
     }
 }
