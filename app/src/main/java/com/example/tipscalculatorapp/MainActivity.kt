203f2c91b782bea738ceaa07e22738e55581bbdf
Sidia <sidia.reis1@gmail.com>
2022-02-10 18:17:35 -0800

    fix:Hide Keybord

--- app/src/main/java/com/example/tipscalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculatorapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tipscalculatorapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tipscalculatorapp.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -22,10 +26,19 @@ class MainActivity : AppCompatActivity() {
         binding.btnCalculateTip.setOnClickListener {
             calculateTip()
         }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
+
+
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.edtServiceCoast.text.toString()
+        val stringInTextField = binding.edtServiceCoast.toString()
         val cost = stringInTextField.toDoubleOrNull()
 
         if (cost == null || cost == 0.0) {
@@ -54,4 +67,16 @@ class MainActivity : AppCompatActivity() {
         val formattedTip = NumberFormat.getCurrencyInstance().format(0.0)
         binding.tvTipResult.text = getString(R.string.tip_amount, formattedTip)
     }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
 }
